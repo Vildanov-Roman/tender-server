59e0a4595061683dffc7ff93177bbe80e0154636
Roman Vildanov <vildanovv.rr@gmail.com>
2025-08-21 15:41:42 +0300

    Update tenderRoutes.js

--- routes/tenderRoutes.js
+++ routes/tenderRoutes.js
@@ -4,17 +4,24 @@ import {
     createTender,
     updateComment,
     deleteTender,
-    archiveTender, getArchivedTenders, deleteArchivedTender, downloadTenderDocument
+    archiveTender,
+    getArchivedTenders,
+    deleteArchivedTender,
 } from '../controllers/tenderController.js';
 
 const router = express.Router();
 
+// /api/tenders
 router.get('/', getAllTenders);
 router.post('/', createTender);
+
+// /api/tenders/archive
 router.get('/archive', getArchivedTenders);
+router.patch('/archive/:tenderId', archiveTender);
 router.delete('/archive/:tenderId', deleteArchivedTender);
+
+// /api/tenders/:tenderId
 router.patch('/:tenderId/comment', updateComment);
 router.delete('/:tenderId', deleteTender);
-router.patch('/archive/:tenderId', archiveTender);
-router.get('/:tenderId/documents/:docId/download', downloadTenderDocument); //Загрузка доков
+
 export default router;
